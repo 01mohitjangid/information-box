5c1e94fcaaaa804132be6e7339d0b06ce2faca25
MOHIT JANGID <116415160+01mohitjangid@users.noreply.github.com>
2023-07-03 10:38:40 +0530

    Add files via upload
    it is a simple information box and i also add backend to save the data of the user

---
+++ info box/script.php
@@ -0,0 +1,17 @@
+<?php
+// Retrieve user input from the form
+$name = $_POST['name'];
+$college = $_POST['college'];
+$branch = $_POST['branch'];
+$batch = $_POST['batch'];
+$github = $_POST['github'];
+$linkedin = $_POST['linkedin'];
+$skills = $_POST['skills'];
+$phone = $_POST['phone'];
+$password = $_POST['password'];
+
+// TODO: Save the information in the backend (e.g., database, file, etc.)
+
+// Display a success message
+echo "Information saved successfully!";
+?>
